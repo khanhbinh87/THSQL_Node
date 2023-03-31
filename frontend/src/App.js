7945c8de5ccf79598d545d068bc1689dfefc847f
khanhbinh <khanhbinh1186>
2023-03-31 17:40:57 +0700

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import './App.css'
-
+import './App.scss'
+// import Nav from './components/Navigation/Nav'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/Login/Login'
 function App() {
     return (
-        <div className='App'>
-            <h1>Hello world React</h1>
-        </div>
+        <Router>
+            {/* <Nav /> */}
+            <Switch>
+                <Route path='/' exact>
+                    Home
+                </Route>
+                <Route path='/news'>News</Route>
+                <Route path='/about'>About</Route>
+                <Route path='/login'><Login /></Route>
+                <Route path='/contact'>Contact</Route>
+                <Route path='*'>404 Not Found</Route>
+            </Switch>
+        </Router>
     )
 }
 
