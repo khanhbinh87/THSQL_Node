2fd8b678e9d1a19ec4bf9c85faac883c289f62ee
khanhbinh <khanhbinh1186>
2023-04-01 16:05:29 +0700

    api register

--- backend/src/controller/apiController.js
+++ backend/src/controller/apiController.js
@@ -9,20 +9,20 @@ const handleRegister = async (req, res) => {
     
     //check email password phone
     try {
-        // if (!req.body.email || !req.body.email.phone || !req.body.email.password) {
-        //     return res.status(200).json({
-        //         EM: 'Miss information',
-        //         EC: '1',
-        //         DT: '',
-        //     })
-        // }
-        // if (req.body.password && req.body.password.length < 4) {
-        //     return res.status(200).json({
-        //         EM: 'Your password must have more than 3  letters',
-        //         EC: '1',
-        //         DT: '',
-        //     })
-        // }
+        if (!req.body.email || !req.body.phone || !req.body.password) {
+            return res.status(200).json({
+                EM: 'Miss information',
+                EC: '1',
+                DT: '',
+            })
+        }
+        if (req.body.password && req.body.password.length < 4) {
+            return res.status(200).json({
+                EM: 'Your password must have more than 3  letters',
+                EC: '1',
+                DT: '',
+            })
+        }
         let data = await loginRegisterService.registerNewUser(req.body)
         console.log('data',data);
         return res.status(200).json({
@@ -38,7 +38,15 @@ const handleRegister = async (req, res) => {
         })
     }
 }
+const handleLogin = async(req,res)=>{
+    console.log(req.body);
+    return res.status(200).json({
+        message:'ok',
+        data:'test'
+    })
+}
 module.exports = {
     testApi,
     handleRegister,
+    handleLogin
 }
